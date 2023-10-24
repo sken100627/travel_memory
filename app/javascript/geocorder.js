5c5f8adcc1925bfa17277a911737e1a0217dbb74
sken100627 <142469920+sken100627@users.noreply.github.com>
2023-10-24 15:33:45 +0900

    geocorder.js編集

--- app/javascript/geocorder.js
+++ app/javascript/geocorder.js
@@ -1,5 +1,5 @@
 export function initMap() {
-  const tower = { lat: 35.6586, lng: 139.7454 }; // 東京タワーを指定
+  const tower = { lat: destination.latitude, lng: destination.latitude };
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 18,
     center: tower,
