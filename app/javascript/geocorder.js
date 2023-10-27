9a823c05bce7531c2e4318f4c4674eee0f56e7fc
sken100627 <142469920+sken100627@users.noreply.github.com>
2023-10-27 15:11:54 +0900

    googlemap表示

--- app/javascript/geocorder.js
+++ app/javascript/geocorder.js
@@ -1,5 +1,5 @@
-export function initMap() {
-  const tower = { lat: destination.latitude, lng: destination.latitude };
+document.addEventListener('turbo:load', function initMap() {
+  const tower = { lat: 35.6586, lng: 139.7454 }; // 東京タワーを指定
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 18,
     center: tower,
@@ -8,6 +8,6 @@ export function initMap() {
     position: tower,
     map: map,
   });
-}
+})
 
 window.initMap = initMap
