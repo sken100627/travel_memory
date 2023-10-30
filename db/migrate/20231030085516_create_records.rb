b34c42af4403be390ae13e45fb6c8d5bfd5d80fc
sken100627 <142469920+sken100627@users.noreply.github.com>
2023-10-30 18:03:10 +0900

    recordモデル作成

---
+++ db/migrate/20231030085516_create_records.rb
@@ -0,0 +1,14 @@
+class CreateRecords < ActiveRecord::Migration[7.0]
+  def change
+    create_table :records do |t|
+      t.integer :gasoline_cost, null: false
+      t.integer :accommondation_fee, null: false
+      t.integer :express_fee, null: false
+      t.integer :total, null: false
+      t.references :user, foreign_key: true
+      t.references :destination, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
