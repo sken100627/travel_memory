e7fa9f331807913fc9e720d576e28366e3e3130a
sken100627 <142469920+sken100627@users.noreply.github.com>
2023-11-10 14:39:04 +0900

    departuresテーブル作成

--- db/migrate/20231110052958_create_departures.rb
+++ db/migrate/20231110052958_create_departures.rb
@@ -1,6 +1,11 @@
 class CreateDepartures < ActiveRecord::Migration[7.0]
   def change
     create_table :departures do |t|
+      t.string :place, null: false, default: ""
+      t.string :address, null: false, default: ""
+      t.float :latitude
+      t.float :longitude
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
