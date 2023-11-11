fb11962575ddb1ead98f10b6ed640c09ff2f36dd
sken100627 <142469920+sken100627@users.noreply.github.com>
2023-11-11 12:24:36 +0900

    createアクション追加

--- app/controllers/departures_controller.rb
+++ app/controllers/departures_controller.rb
@@ -2,4 +2,19 @@ class DeparturesController < ApplicationController
   def new
     @departure = Departure.new
   end
+
+  def create
+    @departure = Departure.new(departure_params)
+    if @departure.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def departure_params
+    params.require(:departure).permit(:place, :address).merge(user_id: current_user.id)
+  end
 end
