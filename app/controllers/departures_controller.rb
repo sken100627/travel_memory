fa057e0ed6a302c36fdb8c0b846a02bb6dfa8dec
sken100627 <142469920+sken100627@users.noreply.github.com>
2023-11-12 16:34:26 +0900

    距離表示

--- app/controllers/departures_controller.rb
+++ app/controllers/departures_controller.rb
@@ -1,10 +1,16 @@
 class DeparturesController < ApplicationController
-  def index
+
+  def show
+    destination = Destination.find(params[:destination_id])
+    departure = Departure.find(params[:id])
+
     @distance = Geocoder::Calculations.distance_between([destination.latitude,destination.longitude],[departure.latitude,departure.longitude]).round
+
   end
 
   def new
     @departure = Departure.new
+    @destination = Destination.find(params[:destination_id])
   end
 
   def create
@@ -19,6 +25,6 @@ class DeparturesController < ApplicationController
   private
 
   def departure_params
-    params.require(:departure).permit(:spot, :location).merge(user_id: current_user.id)
+    params.require(:departure).permit(:spot, :location).merge(user_id: current_user.id, destination_id: params[:destination_id])
   end
 end
--- app/models/departure.rb
+++ app/models/departure.rb
@@ -1,4 +1,9 @@
 class Departure < ApplicationRecord
   geocoded_by :location
   after_validation :geocode, if: :location_changed?
+
+  belongs_to :destination
+  belongs_to :user
+
+  
 end
