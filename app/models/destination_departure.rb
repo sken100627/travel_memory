544affabfda9891b52f328c298892da90be4629f
sken100627 <142469920+sken100627@users.noreply.github.com>
2023-11-13 18:32:19 +0900

    saveメソッド作成

--- app/models/destination_departure.rb
+++ app/models/destination_departure.rb
@@ -1,4 +1,12 @@
 class DestinationDeparture
   include ActiveModel::Model
   attr_accessor :place, :adress, :latitude, :longtitude, :start_date, :end_date, :user_id, :spot, :location, :destination_id
+
+  def save
+    # 寄付情報を保存し、変数donationに代入する
+    destination = Destination.create(place: place, adress: adress, latitude: latitude, longitude: longtitude, start_date: start_date, end_date: end_date, user_id: user_id)
+    # 住所を保存する
+    # donation_idには、変数donationのidと指定する
+    Departure.create(spot: spot, location: location, latitude: latitude, longitude: longtitude, user_id: user_id, destination_id: destination.id)
+  end
 end
