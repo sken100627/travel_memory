e5e677afdab6bca04cfe66984c669baa7920b48c
sken100627 <142469920+sken100627@users.noreply.github.com>
2023-11-13 18:25:50 +0900

    formオブジェクトカラム設定

---
+++ app/models/destination_departure.rb
@@ -0,0 +1,4 @@
+class DestinationDeparture
+  include ActiveModel::Model
+  attr_accessor :place, :adress, :latitude, :longtitude, :start_date, :end_date, :user_id, :spot, :location, :destination_id
+end
