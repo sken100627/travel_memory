f323f7b3eb71c1b035973ba043cd4f0016a3fcd6
sken100627 <142469920+sken100627@users.noreply.github.com>
2023-11-13 18:18:05 +0900

    travelモデル、コントローラー削除

--- app/models/travel.rb
+++
@@ -1,2 +0,0 @@
-class Travel < ApplicationRecord
-end
--- db/migrate/20231113083138_create_travels.rb
+++
@@ -1,8 +0,0 @@
-class CreateTravels < ActiveRecord::Migration[7.0]
-  def change
-    create_table :travels do |t|
-
-      t.timestamps
-    end
-  end
-end
