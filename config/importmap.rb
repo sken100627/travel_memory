64eb40700a9e6089afca71e451472f3cb6870745
sken100627 <142469920+sken100627@users.noreply.github.com>
2023-11-15 21:12:08 +0900

    records-show.js作成

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,4 @@ pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.3/dist/js/bootstrap.e
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.6/lib/index.js"
 pin "geocorder", to: "geocorder.js"
 pin "record", to: "record.js"
+pin "records-show", to: "records-show.js"
